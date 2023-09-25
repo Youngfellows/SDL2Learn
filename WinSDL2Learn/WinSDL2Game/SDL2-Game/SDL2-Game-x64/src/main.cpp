c9598bc703f903134cc3383f71851c3cf1df4a73
Youngfellow <Youngfellow@163.com>
2023-09-25 14:38:47 +0800

    xxxx

---
+++ WinSDL2Learn/WinSDL2Game/SDL2-Game/SDL2-Game-x64/src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+#include "gamemaster.h"
+
+using namespace std;
+
+int main(int argc, char **argv) {
+	start();
+	return 1;
+}
