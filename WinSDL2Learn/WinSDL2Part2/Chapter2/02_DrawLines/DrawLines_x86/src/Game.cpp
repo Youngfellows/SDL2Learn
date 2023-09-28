d66c13e21db1a0db71958379f1c6848803925021
Youngfellow <Youngfellow@163.com>
2023-09-28 10:54:49 +0800

    xxxxx

---
+++ WinSDL2Learn/WinSDL2Part2/Chapter2/02_DrawLines/DrawLines_x86/src/Game.cpp
@@ -0,0 +1,114 @@
+#include "Game.h"
+
+namespace Dungeon
+{
+#define WIDTH 600
+#define HEIGHT 500
+#define FRAMERATE 60
+
+	Game::Game() : mWindow(nullptr), mIsRunning(true), mRenderer(nullptr)
+	{
+
+	}
+
+	bool Game::Initialize()
+	{
+		// SDL库初始化
+		if (SDL_Init(SDL_INIT_VIDEO) != 0)
+		{
+			SDL_Log("Can not init video: %s", SDL_GetError());
+			return false;
+		}
+
+		// 窗口初始化
+		mWindow = SDL_CreateWindow("Dungeon", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
+		if (!mWindow)
+		{
+			SDL_Log("Can not create window: %s", SDL_GetError());
+			return false;
+		}
+
+		// Renderer初始化
+		mRenderer = SDL_CreateRenderer(mWindow, -1, SDL_RENDERER_ACCELERATED);
+		if (!mRenderer)
+		{
+			SDL_Log("Can not create renderer: %s", SDL_GetError());
+			return false;
+		}
+
+		return true;
+	}
+
+	void Game::Shutdown()
+	{
+		SDL_DestroyRenderer(mRenderer);
+		SDL_DestroyWindow(mWindow);
+		SDL_Quit();
+	}
+
+	void Game::Loop()
+	{
+		while (mIsRunning)
+		{
+			uint32_t begin = SDL_GetTicks();
+
+			Draw();
+			Event();
+
+			long current = SDL_GetTicks();
+			long cost = current - begin;
+			long frame = 1000 / FRAMERATE;//每一帧多长时间
+			long delay = frame - cost;
+			//SDL_Log("delay=%ld", delay);
+			if (delay > 0)
+			{
+				Update(delay);
+			}
+		}
+	}
+
+	void Game::Event()
+	{
+		SDL_Event event;
+		// 如果事件队列中有未处理的事件，按顺序处理
+		while (SDL_PollEvent(&event))
+		{
+			switch (event.type)
+			{
+			case SDL_QUIT:// 退出事件，按下窗口的叉
+				mIsRunning = false;
+				break;
+			default:
+				break;
+			}
+		}
+	}
+
+	void Game::Update(long delay)
+	{
+		if (delay > 0)
+		{
+			SDL_Delay(delay);
+		}
+	}
+
+	void Game::Draw()
+	{
+		SDL_SetRenderDrawColor(mRenderer, 255, 255, 2555, 255);
+		SDL_RenderClear(mRenderer);
+
+		SDL_SetRenderDrawColor(mRenderer, 0, 0, 0, 255);
+		SDL_RenderDrawLine(mRenderer, 50, 50, 200, 200);
+
+		SDL_SetRenderDrawColor(mRenderer, 255, 0, 0, 255);
+		SDL_Point points[] = {
+			{50,50},
+			{50,100},
+			{200,300},
+			{300,400}
+		};
+		SDL_RenderDrawLines(mRenderer, points, 4);
+
+		SDL_RenderPresent(mRenderer);
+	}
+}
