c1831d42be338229672fc4dcb79e535c667e5a7d
Youngfellow <Youngfellow@163.com>
2023-10-30 15:15:44 +0800

    xxxx

--- WinSDL2Learn/WinSDL2Part2/Chapter4/11_AudioPlayer/FileReader/src/Main.cpp
+++ WinSDL2Learn/WinSDL2Part2/Chapter4/11_AudioPlayer/FileReader/src/Main.cpp
@@ -8,40 +8,43 @@ using namespace std;
 #define	SRC_FILE_NAME "./resources/readme.txt" //读取文本文件没有问题
 #define SAVE_FILE_NAME "../x64/Debug/save_readme.txt"
 
-#define CACHE_SIZE 16384 //临时缓存大小
+#define CACHE_SIZE 1024 //临时缓存大小
 
 int main(int argc, char *argv[])
 {
 	FILE *srcFile = fopen(SRC_FILE_NAME, "rb");
 	if (!srcFile)
 	{
-		printf("open file error:%s", SRC_FILE_NAME);
+		printf("open file error:%s\n", SRC_FILE_NAME);
 		return 0;
 	}
 	long size = getFileSize(srcFile);
-	printf("size:%ld", size);
+	printf("size:%ld\n", size);
 
 	//保存整个文件的缓存空间
 	char *buffer = (char *)malloc(sizeof(char) * size);
 	if (!buffer)
 	{
-		printf("malloc buffer error");
+		printf("malloc buffer error\n");
 		return 0;
 	}
 	//每一次读取到的缓存空间
 	char *cache = (char *)malloc(sizeof(char) * CACHE_SIZE);
 	if (!cache)
 	{
-		printf("malloc cache error");
+		printf("malloc cache error\n");
 		return 0;
 	}
 	size_t len = 0;
 	int index = 0;
-	while (len = fread(cache, 1, CACHE_SIZE, srcFile) > 0)
+	while ((len = fread(cache, 1, CACHE_SIZE, srcFile)) > 0)
 	{
+		printf("index:%d,len:%ld\n", index, len);
 		strcpy(buffer + index, cache);//把读取到的数据拷贝到缓存
+		//strncpy(buffer + index, cache, len);
 		//memcpy(buffer + index, cache, len);
 		index += len;
+		//printf("%s", cache);
 		memset(cache, 0, CACHE_SIZE);//清空缓存
 	}
 	fclose(srcFile);
