394ada608c68c4e991b420f830a6e269a48f9733
Youngfellow <Youngfellow@gmail.com>
2023-11-04 14:09:24 +0800

    xxxx

---
+++ WinSDL2Learn/WinSDL2Part1/Day10/InlinePhoto-x86/include/EventListener.h
@@ -0,0 +1,27 @@
+#ifndef _DUNGEON_EVENT_LISTENER_H_
+#define _DUNGEON_EVENT_LISTENER_H_
+
+#include "DisplayObject.h"
+
+namespace Dungeon
+{
+	/*
+	* 1. 注册的事件监听回调、
+	* 2. 如果需要调用其他对象的函数,需要在回调中传参该对象
+	*/
+	class EventListener
+	{
+	private:
+
+	public:
+		EventListener();
+		~EventListener();
+		static void OnClickRect(DisplayObject *self);//点击矩形的回调
+		static void OnClickCat();//点击矩形的回调
+		static void OnClickText1(DisplayObject *self);//点击文本1
+		static void OnClickText2(DisplayObject *self);//点击文本2
+	};
+
+}
+#endif // !_DUNGEON_EVENT_LISTENER_H_
+
--- WinSDL2Learn/WinSDL2Part1/Day10/InlinePhoto-x86/include/InlinePhoto.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-void draw(SDL_Surface* screen, SDL_Window* win);
-void event_loop(SDL_Surface* screen, SDL_Window* win);
