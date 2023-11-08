9ff063103033d661ac25898338cc47f644501a8e
Youngfellow <Youngfellow@163.com>
2023-11-08 17:46:08 +0800

    xxxxxx

---
+++ WinSDL2Learn/WinSDL2Part4/07_SDLWindow/SDLWindow_x64/src/Main.cpp
@@ -0,0 +1,59 @@
+﻿#include <string>
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include "Window.h"
+
+int main(int argc, char *argv[])
+{
+	//Show A Window
+	//In main()
+	//Load up an image and some text
+	SDL_Texture *img, *msg;
+	try {
+		//Start our window
+		Window::Init("Shiver's Window");
+
+		int angle = 0;
+
+		Window::HandleEvents([](SDL_Event e)
+			{
+				switch (e.key.keysym.sym) {
+				case SDLK_q:
+				case SDLK_ESCAPE:
+					Window::m_eventQuit = true;
+					break;
+				}
+				//按下按键a,显示文本
+				if (e.key.keysym.sym == SDLK_a) {
+					printf("keydown A \n");
+					Window::Clear();
+					std::string fontFile = "./resources/sample.ttf";
+					std::string text = "TTF Fonts too!";
+					SDL_Color color = { 255, 255, 255 };
+					SDL_Texture *texture = Window::RenderText(text, fontFile, color, 25);
+					SDL_DestroyTexture(texture);
+				}
+
+				//按下按键f,显示图片
+				if (e.key.keysym.sym == SDLK_f) {
+					printf("keydown F \n");
+					Window::Clear();
+					std::string imgFile = "./resources/image.png";
+					SDL_Texture *image = Window::LoadImage(imgFile);
+					SDL_DestroyTexture(image);
+				}
+			});
+
+
+
+		Window::Quit();
+	}
+	catch (const std::runtime_error &e) {
+		//Catch error and crash
+		std::cout << e.what() << std::endl;
+		Window::Quit();
+		return -1;
+	}
+	return 0;
+}
---
+++ WinSDL2Learn/WinSDL2Part4/11_RenderImage/RenderImage_x64/include/res_path.h
@@ -0,0 +1,47 @@
+#ifndef RES_PATH_H
+#define RES_PATH_H
+
+#include <iostream>
+#include <string>
+#include <SDL2/SDL.h>
+
+/*
+ * Get the resource path for resources located in res/subDir
+ * It's assumed the project directory is structured like:
+ * bin/
+ *  the executable
+ * res/
+ *  Lesson1/
+ *  Lesson2/
+ *
+ * Paths returned will be Project_Root/res/subDir
+ */
+std::string getResourcePath(const std::string &subDir = "")
+{
+#ifdef _WIN32
+	const char PATH_SEP = '\\';
+#else
+	const char PATH_SEP = '/';
+#endif
+	static std::string baseRes;
+	if (baseRes.empty()) {
+		char *basePath = SDL_GetBasePath();
+		SDL_Log("1,basePath:%s", basePath);
+		if (basePath) {
+			baseRes = basePath;
+			SDL_Log("2,baseRes:%s", baseRes.c_str());
+			SDL_free(basePath);
+		}
+		else {
+			std::cerr << "Error getting resource path: " << SDL_GetError() << std::endl;
+			return "";
+		}
+		//We replace the last bin/ with res/ to get the the resource path
+		size_t pos = baseRes.rfind("bin");
+		baseRes = baseRes.substr(0, pos) + "res" + PATH_SEP;
+		SDL_Log("3,baseRes:%s", baseRes.c_str());
+	}
+	return subDir.empty() ? baseRes : baseRes + subDir + PATH_SEP;
+}
+
+#endif
