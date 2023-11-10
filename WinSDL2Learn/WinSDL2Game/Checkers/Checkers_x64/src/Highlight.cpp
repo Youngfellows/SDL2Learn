ba31ef62db72a1fb908effd49e5de03960c4a062
Youngfellow <Youngfellow@163.com>
2023-11-10 15:30:54 +0800

    xxxxx

---
+++ WinSDL2Learn/WinSDL2Game/Checkers/Checkers_x64/src/Highlight.cpp
@@ -0,0 +1,38 @@
+#include "Highlight.h"
+
+SDL_Renderer *Highlight::sRenderer = nullptr;
+SDL_Color Highlight::sColourCache[sTotalColours];
+
+Highlight::Highlight()
+	:mHighlightType(HighlightType::NONE), mColour(&sColourCache[(int)mHighlightType]), mRect({ 0, 0, 0, 0 })
+{
+
+}
+
+void Highlight::setRectTo(const SDL_Rect &rect)
+{
+	mRect = rect;
+}
+
+void Highlight::setHighlightTo(HighlightType highlightType)
+{
+	mHighlightType = highlightType;
+	mColour = &sColourCache[(int)highlightType];
+}
+
+HighlightType Highlight::getHighlight()
+{
+	return mHighlightType;
+}
+
+void Highlight::renderHighlight()
+{
+	// Only render highlight if square is not empty
+	if (mHighlightType != HighlightType::NONE)
+	{
+		SDL_SetRenderDrawBlendMode(sRenderer, SDL_BLENDMODE_BLEND);
+		SDL_SetRenderDrawColor(sRenderer, mColour->r, mColour->g, mColour->b, mColour->a);
+		SDL_RenderFillRect(sRenderer, &mRect);
+	}
+
+}
