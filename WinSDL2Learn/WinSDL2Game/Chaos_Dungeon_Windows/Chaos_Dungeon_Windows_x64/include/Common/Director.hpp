2d0a2fa03d30d95c0de64d54b5a36352ebd825c6
Youngfellow <Youngfellow@163.com>
2023-11-13 13:00:21 +0800

    xxxxxx

--- WinSDL2Learn/WinSDL2Game/Chaos_Dungeon_Windows/Chaos_Dungeon_Windows_x64/include/Common/Director.hpp
+++ WinSDL2Learn/WinSDL2Game/Chaos_Dungeon_Windows/Chaos_Dungeon_Windows_x64/include/Common/Director.hpp
@@ -8,11 +8,17 @@
 #include <map>
 using namespace std;
 
-enum KeyState{
-    PRESSED,    /** 在这一帧中才按下*/
-    PRESSING,   /** 已经按下超过一帧了*/
-    RELEASED,   /** 在这一帧中才松开*/
-    RELEASING   /** 已经松开超过一帧了*/
+//enum KeyState{PRESSED,/** 在这一帧中才按下*/
+//PRESSING,/** 已经按下超过一帧了*/
+//RELEASED,/** 在这一帧中才松开*/
+//RELEASING/** 已经松开超过一帧了*/
+//};
+enum KeyState
+{
+PRESSED,
+PRESSING,
+RELEASED,
+RELEASING
 };
 
 class Director{
