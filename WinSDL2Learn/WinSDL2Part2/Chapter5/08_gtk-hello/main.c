3412d37284da4d5488831f288bf2b66e3f212253
Youngfellow <Youngfellow@163.com>
2023-11-24 17:42:16 +0800

    xxxx

--- WinSDL2Learn/WinSDL2Part2/Chapter5/08_gtk-hello/main.c
+++ WinSDL2Learn/WinSDL2Part2/Chapter5/08_gtk-hello/main.c
@@ -2,7 +2,7 @@
 
 static void print_hello (GtkWidget *widget, gpointer   data)
 {
- g_print ("Hello World\n");
+    g_print ("Hello World\n");
 }
 
 static void activate(GtkApplication *app, gpointer user_data)
@@ -25,12 +25,12 @@ static void activate(GtkApplication *app, gpointer user_data)
 int main(int argc, char **argv)
 {
     GtkApplication *app;
-         int status;
+    int status;
 
- app = gtk_application_new("org.gtk.helloword", G_APPLICATION_FLAGS_NONE);
- g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
- status = g_application_run(G_APPLICATION(app), argc, argv);
- g_object_unref(app);
+    app = gtk_application_new("org.gtk.helloword", G_APPLICATION_FLAGS_NONE);
+    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+    status = g_application_run(G_APPLICATION(app), argc, argv);
+    g_object_unref(app);
 
  return status;
 }
